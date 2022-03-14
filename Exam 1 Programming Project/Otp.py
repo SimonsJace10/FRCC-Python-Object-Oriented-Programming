8903f8025cd22b0621476ee5c2ea9de541a76c76
SimonsJace10 <Simons.Jace10@svvsd.org>
2022-03-13 19:50:08 -0600

    exam 1 complete

--- Exam 1 Programming Project/Otp.py
+++ Exam 1 Programming Project/Otp.py
@@ -16,21 +16,41 @@
 from secrets import token_bytes
 from typing import Tuple
 
+
 class Otp(object):
     def __init__(self, m):
         self.msg = m
 
-    @staticmethod 
-    def __random_key(length):
+    @staticmethod
+    def random_key(length):
         # generate length random bytes
         tb = token_bytes(length)
         # convert those bytes into a bit string and return it
         return int.from_bytes(tb, "big")
+
+    # the encrypt method can't be static and needs self as an argument because it needs to access the member method of
+    # random_key in order to generate a dummy key
+    def encrypt(self):
+        original_bytes = self.msg.encode()
+        dummy = self.random_key(len(original_bytes))
+        original_key = int.from_bytes(original_bytes, "big")
+        encrypted = original_key ^ dummy  # XOR
+        return dummy, encrypted
+
+    # the static decrypt method provides a decryption using the passed in keys, which are returned by the encrypt()
+    # method above
+    @staticmethod
+    def decrypt(key1, key2):
+        decrypted = key1 ^ key2  # XOR
+        temp = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, "big")
+        return temp.decode()
        
 
 if __name__ == "__main__":
     secretMsg = Otp("One Time Pad!")
-    secretMsg.encrypt()
-    print (secretMsg.decrypt())
-    
-    print(secretMsg)
+    # print the passed in string to confirm that it revealed the correct message
+    print(secretMsg.msg)
+    # create a tuple that stores the two returned keys from the encrypt() method
+    returned_keys = (secretMsg.encrypt())
+    # print the decrypted string, by calling the decrypt method and passing in the keys stored in the tuple
+    print("secret message is: " + secretMsg.decrypt(returned_keys[0], returned_keys[1]))
